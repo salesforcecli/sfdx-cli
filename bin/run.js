7723102ccfb391a7feadd3ca9bfe6e19a3681cc6
Bob Bergman <rbergman@salesforce.com>
2017-09-11 10:29:32 -0600

    Turn off updates being disabled by default

--- bin/run.js
+++ bin/run.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const root = path.join(__dirname, '..');
+const path = require("path");
+const root = path.join(__dirname, "..");
 
-// check node version before requiring additional packages
-require(path.join(root, 'lib', 'versions'))
+// Check node version before requiring additional packages
+require(path.join(root, "lib", "versions"))
     .checkNodeVersion();
 
-const pjson = require(path.join(root, 'package.json'));
-require(path.join(root, 'lib', 'cli'))
+const pjson = require(path.join(root, "package.json"));
+require(path.join(root, "lib", "cli"))
     .create(pjson.version, pjson.cli.channel)
     .run();
