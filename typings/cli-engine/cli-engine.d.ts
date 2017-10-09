31f2d667a620a021f9fc87679c74ee73847332ac
Bob Bergman <rbergman@salesforce.com>
2017-10-09 13:05:58 -0600

    Typings fixes and additions

--- typings/cli-engine/cli-engine.d.ts
+++ typings/cli-engine/cli-engine.d.ts
@@ -1,6 +1,25 @@
 declare module 'cli-engine' {
+    import { Config } from 'cli-engine-config';
+
     export default class CLI {
-        constructor({ argv, config }: { argv: string[], config: any });
+        constructor({ argv, config }: { argv: string[], config: Config });
         public run(): void;
+    }
+}
+
+declare module 'cli-engine/lib/lock' {
+    import { Config } from 'cli-engine-config';
+    import CLI from 'cli-engine';
+
+    export default class Lock {
+        config: Config;
+        cli: CLI;
+
+        public constructor(config: Config);
+        public updatelockfile(): string;
+        public read();
+        public unread();
+        public canRead();
+        public upgrade();
     }
 }
