dc899c99b4a7174d9a1fc7001db6dfdd44d9a3e6
Thomas <t.dvornik@salesforce.com>
2017-10-24 13:40:53 -0600

    Update analytics with spawn

--- src/hooks/logAnalytics.ts
+++ src/hooks/logAnalytics.ts
@@ -1,9 +1,9 @@
 import { Config } from 'cli-engine-config';
+import * as cp from 'child_process';
 import * as Debug from 'debug';
+import * as fs from 'fs';
 import * as path from 'path';
 import timedHook from './timedHook';
-
-import * as cp from 'child_process';
 
 const debug = Debug('sfdx:analytics');
 
@@ -14,14 +14,27 @@ function run(config: Config, opts: any) {
 
         // Only log usage for commands with plugins
         if (command && command.plugin) {
+            debug('setting up exit handler');
             process.on('exit', (status) => {
-                cp.fork(path.join(__dirname, '../processes/logUsage'), [], { execArgv: [] }).send({
-                    config,
-                    plugin: command.plugin ? { name: command.plugin.name, version: command.plugin.version } : undefined,
-                    commandId: command.id,
-                    time: Date.now() - start,
-                    status
-                });
+                const logFile = path.join(config.cacheDir as string, 'analytics.log');
+                debug(`using ${logFile}} for usage error logging`);
+
+                let fd = fs.openSync(logFile, 'a');
+
+                cp.spawn(process.argv[0], [
+                    path.join(__dirname, '../processes/logUsage'),
+                    JSON.stringify({
+                        config,
+                        plugin: command.plugin ? { name: command.plugin.name, version: command.plugin.version } : undefined,
+                        commandId: command.id,
+                        time: Date.now() - start,
+                        status
+                    })
+                ], {
+                    detached: !config.windows,
+                    stdio: ['ignore', fd, fd]
+                }).unref();
+                debug(`spawned usage "${process.argv[0]} ${path.join(__dirname, '../processes/logUsage')}"`);
             });
         } else {
             debug('no plugin found for analytics');
