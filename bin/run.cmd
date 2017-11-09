1d310ff0a9eceab7921b4e09e65326efd6936392
Bob Bergman <rbergman@salesforce.com>
2017-11-09 15:35:12 -0700

    More potential windows cmd script fixes

--- bin/run.cmd
+++ bin/run.cmd
@@ -1,12 +1,12 @@
 @echo off
 
-set SFDX_BINARY=false & set BIN_NAME=run
+set SFDX_BINARY=false&set BIN_NAME=run
 REM @OVERRIDES@
 if "%SFDX_BINARY%" == "true" (
     REM installer/update that shipped its own node binary
-    set CLI_BINPATH=%~dp0\%BIN_NAME%.cmd
-    "%~dp0\node.exe" "%~dp0\%BIN_NAME%.js" %*
+    set CLI_BINPATH=%~dp0%BIN_NAME%.cmd
+    "%~dp0node.exe" "%~dp0%BIN_NAME%.js" %*
 ) else (
     REM npm install or local dev
-    node "%~dp0\run.js" %*
+    node "%~dp0run.js" %*
 )
--- resources/exe/sfdx.cmd
+++ resources/exe/sfdx.cmd
@@ -1,9 +1,9 @@
 @echo off
 
-set CLI_BINPATH=%~dp0\sfdx.cmd
+set CLI_BINPATH=%~dp0sfdx.cmd
 
 if exist "%LOCALAPPDATA%\sfdx\client\bin\sfdx.cmd" (
   "%LOCALAPPDATA%\sfdx\client\bin\sfdx.cmd" %*
 ) else (
-  "%~dp0\..\client\bin\node.exe" "%~dp0\..\client\bin\sfdx.js" %*
+  "%~dp0..\client\bin\node.exe" "%~dp0..\client\bin\sfdx.js" %*
 )
