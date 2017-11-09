5107a02b87edb656c5d4dd1ea73007f0cc1640f2
Bob Bergman <rbergman@salesforce.com>
2017-11-09 15:29:02 -0700

    Potential sfdx.cmd fix

--- bin/run.cmd
+++ bin/run.cmd
@@ -2,7 +2,7 @@
 
 set SFDX_BINARY=false & set BIN_NAME=run
 REM @OVERRIDES@
-if (%SFDX_BINARY% == "true") (
+if "%SFDX_BINARY%" == "true" (
     REM installer/update that shipped its own node binary
     set CLI_BINPATH=%~dp0\%BIN_NAME%.cmd
     "%~dp0\node.exe" "%~dp0\%BIN_NAME%.js" %*
