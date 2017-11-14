d0afe6b6d9d70c880f576ba87ae7cb70c6917927
Bob Bergman <rbergman@salesforce.com>
2017-11-14 14:51:47 -0700

    Fix win run.cmd script

--- bin/run.cmd
+++ bin/run.cmd
@@ -5,7 +5,7 @@ REM @OVERRIDES@
 if "%SFDX_BINARY%" == "true" (
     REM installer/update that shipped its own node binary
     set CLI_BINPATH=%~dp0%BIN_NAME%.cmd
-    "%~dp0node.exe" "%~dp0%BIN_NAME%.cmd" %*
+    "%~dp0node.exe" "%~dp0%BIN_NAME%" %*
 ) else (
     REM npm install or local dev
     node "%~dp0run" %*
