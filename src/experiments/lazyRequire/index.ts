cca092cd5eb1df77b7c2c29b1a02a1e2d58a6b99
Bob Bergman <rbergman@salesforce.com>
2018-05-15 10:24:59 -0600

    Extract lazy loader to its own npm package

---
+++ src/experiments/lazyRequire/index.ts
@@ -0,0 +1,32 @@
+import path = require('path');
+import fs = require('fs');
+import { type } from 'os';
+import Debug = require('debug');
+import { buildConfig } from 'cli-engine-config';
+import LazyRequire from 'lazy-require';
+
+let lazyRequire: LazyRequire;
+
+/**
+ * Start lazy requiring type-compatible modules.
+ */
+export function start(): void {
+    getOrCreate().start();
+}
+
+/**
+ * Return the lazy require type cache if it has been initialized.
+ */
+export function resetTypeCache(): void {
+    getOrCreate().resetTypeCache();
+}
+
+function getOrCreate(): LazyRequire {
+    if (lazyRequire) {
+        return lazyRequire;
+    }
+    const pjson = require('../../../package.json'); // tslint:disable-line no-var-requires
+    const cacheDir = buildConfig({ bin: pjson['cli-engine'].bin }).cacheDir!;
+    const typeCacheFile = path.join(cacheDir, 'module-types.json');
+    return lazyRequire = LazyRequire.create(typeCacheFile);
+}
