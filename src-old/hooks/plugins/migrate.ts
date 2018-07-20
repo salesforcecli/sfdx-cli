fedf7ea1b7a61cd89b25ec5c96c7ce2fa9f39dcc
Bob Bergman <rbergman@salesforce.com>
2018-07-20 08:51:53 -0600

    wip: remove old bin and src folders

--- src-old/hooks/plugins/migrate.ts
+++
@@ -1,9 +0,0 @@
-import { Config } from 'cli-engine-config';
-import PluginMigrator from '../../legacy/PluginMigrator';
-import timedHook from '../timedHook';
-
-async function run(config: Config): Promise<void> {
-    return await PluginMigrator.run(config);
-}
-
-export = timedHook('init:plugins:migrate', run);
