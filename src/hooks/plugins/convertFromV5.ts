47573e5a1f78b6504e8f88101e8bd574a060998c
Bob Bergman <rbergman@salesforce.com>
2018-07-20 07:44:11 -0600

    wip: salesforcedx works as a core plugin!

--- src/hooks/plugins/convertFromV5.ts
+++ src/hooks/plugins/convertFromV5.ts
@@ -1,62 +1,78 @@
-// TODO: commented out until oclif adds `module` to the `plugins:parse` hook options?
-// import { Command } from '@oclif/command';
-// import { Config } from '@oclif/config';
+import { PluginLegacy } from '@oclif/plugin-legacy';
 // import chalk from 'chalk';
+import { asAnyJson, ensureJsonMap, isJsonMap } from '@salesforce/ts-json';
+import { compact } from '../../util';
 import timedHook from '../timedHook';
 
-// function convertFromV5Commands(commands: any[] = [], ns: string): any[] { // tslint:disable-line:no-any
-//     return commands
-//         .map((cmd: any) => { // tslint:disable-line:no-any
-//             if (cmd.namespace === ns) {
-//                 cmd.topic = ns;
-//                 return cmd;
-//             }
-//             cmd.topic = applyNamespace(cmd.topic, ns);
-//             cmd.buildHelp = (config: Config) => {
-//                 const help = Command.buildHelp.call(cmd, config);
-//                 // Strip the possibly ANSI-colored '[flags]' suffix cli-engine appends to usage strings
-//                 return help.replace(/(?:\u001b\[[0-9]+m)?\[flags\](?:\u001b\[[0-9]+m)?/, '');
-//             };
+function convertFromV5Commands(commands: any[] = [], ns: string): any[] { // tslint:disable-line:no-any
+    return commands
+        .map((cmd: any) => { // tslint:disable-line:no-any
+            if (cmd.namespace === ns) {
+                cmd.topic = ns;
+                return cmd;
+            }
+            cmd.topic = applyNamespace(cmd.topic, ns);
+            // TODO???
+            // cmd.buildHelp = (config: Config) => {
+            //     const help = Command.buildHelp.call(cmd, config);
+            //     // Strip the possibly ANSI-colored '[flags]' suffix cli-engine appends to usage strings
+            //     return help.replace(/(?:\u001b\[[0-9]+m)?\[flags\](?:\u001b\[[0-9]+m)?/, '');
+            // };
 
-//             // Do not use arrow function here because we need access to the command's properties
-//             cmd.buildHelpLine = function(config: Config): [string, string] {
-//                 return [`${this.topic}:${this.command}`, chalk.dim(this.description)];
-//             };
+            // TODO???
+            // Do not use arrow function here because we need access to the command's properties
+            // cmd.buildHelpLine = function(config: Config): [string, string] {
+            //     return [`${this.topic}:${this.command}`, chalk.dim(this.description)];
+            // };
 
-//             return cmd;
-//         });
-// }
+            return cmd;
+        });
+}
 
-// function convertFromV5Topics(topics: any[] = [], ns: string, nsDescription: string): any[] { // tslint:disable-line:no-any
-//     return [{
-//         description: nsDescription,
-//         hidden: false,
-//         name: ns
-//     }].concat(topics.map((topic: any) => { // tslint:disable-line:no-any
-//         topic.name = applyNamespace(topic.name, ns);
-//         return topic;
-//     }));
-// }
+function convertFromV5Topics(topics: any[] = [], ns: string, nsDescription: string): any[] { // tslint:disable-line:no-any
+    return [{
+        description: nsDescription,
+        hidden: false,
+        name: ns
+    }].concat(topics.map((topic: any) => { // tslint:disable-line:no-any
+        topic.name = applyNamespace(topic.name, ns);
+        return topic;
+    }));
+}
 
-// function hasNamespace(name: string, ns: string): boolean {
-//     return !!name && name.indexOf(ns + ':') === 0;
-// }
+function hasNamespace(name: string, ns: string): boolean {
+    return !!name && name.indexOf(ns + ':') === 0;
+}
 
-// function applyNamespace(name: string, ns: string): string {
-//     return !hasNamespace(name, ns) ? `${ns}:${name}` : name;
-// }
+function applyNamespace(name: string, ns: string): string {
+    return !hasNamespace(name, ns) ? `${ns}:${name}` : name;
+}
 
-const hook = timedHook<'plugins:parse'>('plugins:parse:legacy', async options => {
-    console.log('TODO: implement plugins:parse:legacy hook');
-    // if (module.namespace) {
-    //     const ns = module.namespace.name;
-
-    //     module.commands = convertFromV5Commands(module.commands, ns);
-    //     const topics = module.topics || (module.topic && [module.topic]);
-    //     module.topics = convertFromV5Topics(topics, ns, module.namespace.description);
-
-    //     delete module.namespace;
-    // }
+const hook = timedHook<'init'>('init:legacy', async options => {
+    await Promise.all(options.config.plugins.map(async (p, i) => {
+        if (p.valid) return;
+        try {
+            process.env.CLI_ENGINE_VERSION = 'v7';
+            const plugin = new PluginLegacy(options.config, p);
+            const cliEngineConfig = p.pjson['cli-engine'];
+            // TODO: support for direct install of fct and fls???  think they lack this pjson section
+            if (isJsonMap(cliEngineConfig) && cliEngineConfig.namespace) {
+                const module = require(p.name);
+                const ns = module.namespace.name;
+                module.commands = convertFromV5Commands(module.commands, ns);
+                const topics = module.topics || (module.topic && [module.topic]);
+                module.topics = convertFromV5Topics(topics, ns, module.namespace.description);
+                delete module.namespace;
+            }
+            await plugin.load();
+            // TODO: maybe post-inspect loaded plugin for ns and massage?  see fct/fls note above
+            options.config.plugins[i] = plugin;
+        } catch (err) {
+            err.name = `@oclif/plugin-legacy: Plugin ${p.name}: ${err.name}`;
+            err.detail = compact([err.detail, p.root]).join(' ');
+            process.emitWarning(err);
+        }
+    }));
 });
 
 export default hook;
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,4 +1,10 @@
+import { Optional } from '@salesforce/ts-json';
+
 export async function sleep(millis: number): Promise<void> {
     // tslint:disable-next-line:no-string-based-set-timeout https://github.com/Microsoft/tslint-microsoft-contrib/issues/355
     await new Promise(resolve => setTimeout(resolve, millis));
+}
+
+export function compact<T>(a: Array<Optional<T>>): T[] {
+    return a.filter((i): i is T => !!i);
 }
