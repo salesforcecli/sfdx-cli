094f36088267af5fb5710f637671d7547bd42cd8
Bob Bergman <rbergman@salesforce.com>
2018-09-26 16:31:32 -0600

    Remove unused util

--- src/util/index.ts
+++ src/util/index.ts
@@ -5,13 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { Optional } from '@salesforce/ts-types';
-
 export async function sleep(millis: number): Promise<void> {
     // tslint:disable-next-line:no-string-based-set-timeout https://github.com/Microsoft/tslint-microsoft-contrib/issues/355
     await new Promise(resolve => setTimeout(resolve, millis));
-}
-
-export function compact<T>(a: Array<Optional<T>>): T[] {
-    return a.filter((i): i is T => !!i);
 }
