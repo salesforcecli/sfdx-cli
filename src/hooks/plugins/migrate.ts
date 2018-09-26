e77fe13c8d333cfe1f45a8a3149b032f1cc2896e
Bob Bergman <rbergman@salesforce.com>
2018-09-26 16:12:11 -0600

    Add env tests

--- src/hooks/plugins/migrate.ts
+++
@@ -1,9 +0,0 @@
-import PluginMigrator from '../../legacy/PluginMigrator';
-import timedHook from '../timedHook';
-
-const hook = timedHook<'init'>('init:plugins:migrate', async options => {
-    const corePlugins = options.config.plugins.filter(plugin => plugin.type === 'core');
-    await PluginMigrator.run(corePlugins, options.config.dataDir);
-});
-
-export default hook;
--- src/util/env.ts
+++ src/util/env.ts
@@ -1,7 +1,6 @@
 import { Env as KitEnv } from '@salesforce/kit';
-import { Optional } from '@salesforce/ts-types';
 
-export class Env {
+export class Env extends KitEnv {
     public static readonly CLI_MODE = 'SFDX_ENV';
     public static readonly CLI_INSTALLER = 'SFDX_INSTALLER';
     public static readonly DISABLE_AUTOUPDATE_LEGACY = 'SFDX_AUTOUPDATE_DISABLE';
@@ -10,33 +9,8 @@ export class Env {
     public static readonly S3_HOST = 'SFDX_S3_HOST';
     public static readonly LAZY_LOAD_MODULES = 'SFDX_LAZY_LOAD_MODULES';
 
-    public constructor(private env = process.env) {
-        this.env = env;
-    }
-
-    public getString(key: string): Optional<string>;
-    public getString(key: string, def: string): string;
-    public getString(key: string, def?: string): Optional<string> {
-        return this.env[key] || def;
-    }
-
-    public getBoolean(key: string, def: boolean = false): boolean {
-        return this.getString(key, def.toString()).toLowerCase() === 'true';
-    }
-
-    public setString(key: string, val?: string): void {
-        if (val == null) {
-            this.unset(key);
-        }
-        this.env[key] = val;
-    }
-
-    public setBoolean(key: string, val: boolean): void {
-        this.setString(key, val.toString());
-    }
-
-    public unset(key: string): boolean {
-        return delete this.env[key];
+    public constructor(env = process.env) {
+        super(env);
     }
 
     public isAutoupdateDisabled(): boolean {
