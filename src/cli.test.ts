d24f976a647514b5c4d77c040657de306cf54a39
Bob Bergman <rbergman@salesforce.com>
2018-10-04 07:20:17 -0600

    Basic test of cli create fn

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -10,56 +10,66 @@
 import { expect } from 'chai';
 import {
     configureAutoUpdate,
+    create,
     UPDATE_DISABLED_DEMO,
     UPDATE_DISABLED_INSTALLER,
     UPDATE_DISABLED_NPM
 } from './cli';
 import { Env } from './util/env';
 
-describe('CLI flags', () => {
-    let env: Env;
-
-    beforeEach(() => {
-        env = new Env({});
+describe('cli', () => {
+    describe('create', () => {
+        it('should create a runnable CLI instance', () => {
+            const cli = create('test', 'test');
+            expect(cli).to.have.property('run');
+        });
     });
 
-    it('should default to autoupdate disabled for local dev or npm installs', () => {
-        configureAutoUpdate(env);
+    describe('flags', () => {
+        let env: Env;
 
-        expect(env.getBoolean('SFDX_AUTOUPDATE_DISABLE')).to.be.true;
-        expect(env.getString(Env.UPDATE_INSTRUCTIONS)).to.equal(UPDATE_DISABLED_NPM);
-    });
+        beforeEach(() => {
+            env = new Env({});
+        });
 
-    it('should allow autoupdate to be explicitly enabled for local dev (for testing autoupdates)', () => {
-        env.setBoolean('SFDX_AUTOUPDATE_DISABLE', false);
-        configureAutoUpdate(env);
+        it('should default to autoupdate disabled for local dev or npm installs', () => {
+            configureAutoUpdate(env);
 
-        expect(env.getBoolean('SFDX_AUTOUPDATE_DISABLE')).to.be.false;
-        expect(env.getString(Env.UPDATE_INSTRUCTIONS)).to.be.undefined;
-    });
+            expect(env.getBoolean('SFDX_AUTOUPDATE_DISABLE')).to.be.true;
+            expect(env.getString(Env.UPDATE_INSTRUCTIONS)).to.equal(UPDATE_DISABLED_NPM);
+        });
 
-    it('should default to autoupdate enabled for binary installs', () => {
-        env.setBoolean('SFDX_INSTALLER', true);
-        configureAutoUpdate(env);
+        it('should allow autoupdate to be explicitly enabled for local dev (for testing autoupdates)', () => {
+            env.setBoolean('SFDX_AUTOUPDATE_DISABLE', false);
+            configureAutoUpdate(env);
 
-        expect(env.getBoolean('SFDX_AUTOUPDATE_DISABLE')).to.be.false;
-        expect(env.getString(Env.UPDATE_INSTRUCTIONS)).to.be.undefined;
-    });
+            expect(env.getBoolean('SFDX_AUTOUPDATE_DISABLE')).to.be.false;
+            expect(env.getString(Env.UPDATE_INSTRUCTIONS)).to.be.undefined;
+        });
 
-    it('should have autoupdate disabled for binary installs when SFDX_AUTOUPDATE_DISABLE is set to true', () => {
-        env.setBoolean('SFDX_INSTALLER', true);
-        env.setBoolean('SFDX_AUTOUPDATE_DISABLE', true);
-        configureAutoUpdate(env);
+        it('should default to autoupdate enabled for binary installs', () => {
+            env.setBoolean('SFDX_INSTALLER', true);
+            configureAutoUpdate(env);
 
-        expect(env.getBoolean('SFDX_AUTOUPDATE_DISABLE')).to.be.true;
-        expect(env.getString(Env.UPDATE_INSTRUCTIONS)).to.equal(UPDATE_DISABLED_INSTALLER);
-    });
+            expect(env.getBoolean('SFDX_AUTOUPDATE_DISABLE')).to.be.false;
+            expect(env.getString(Env.UPDATE_INSTRUCTIONS)).to.be.undefined;
+        });
 
-    it('should have autoupdate disabled when in demo mode', () => {
-        env.setString('SFDX_ENV', 'DEMO');
-        configureAutoUpdate(env);
+        it('should have autoupdate disabled for binary installs when SFDX_AUTOUPDATE_DISABLE is set to true', () => {
+            env.setBoolean('SFDX_INSTALLER', true);
+            env.setBoolean('SFDX_AUTOUPDATE_DISABLE', true);
+            configureAutoUpdate(env);
 
-        expect(env.getBoolean('SFDX_AUTOUPDATE_DISABLE')).to.be.true;
-        expect(env.getString(Env.UPDATE_INSTRUCTIONS)).to.equal(UPDATE_DISABLED_DEMO);
+            expect(env.getBoolean('SFDX_AUTOUPDATE_DISABLE')).to.be.true;
+            expect(env.getString(Env.UPDATE_INSTRUCTIONS)).to.equal(UPDATE_DISABLED_INSTALLER);
+        });
+
+        it('should have autoupdate disabled when in demo mode', () => {
+            env.setString('SFDX_ENV', 'DEMO');
+            configureAutoUpdate(env);
+
+            expect(env.getBoolean('SFDX_AUTOUPDATE_DISABLE')).to.be.true;
+            expect(env.getString(Env.UPDATE_INSTRUCTIONS)).to.equal(UPDATE_DISABLED_DEMO);
+        });
     });
 });
