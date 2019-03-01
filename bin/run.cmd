514c78702f29c54bb45bb22d3cf14dffa304ee71
Bob Bergman <rbergman@salesforce.com>
2019-03-01 16:53:29 -0700

    update windows launch scripts with correct redirected usage

--- bin/run.cmd
+++ bin/run.cmd
@@ -12,12 +12,11 @@ set SFDX_BINPATH="%~dp0%BIN_NAME%.cmd"
 set LATEST_BINPATH="%LOCALAPPDATA%\%BIN_NAME%\client\bin\%BIN_NAME%.cmd"
 if "%SFDX_INSTALLER%"=="true" (
     REM installer/update that shipped its own node binary
-    if /I %LATEST_BINPATH% == %SFDX_BINPATH% (
+    if "%SFDX_REDIRECTED%" == "1" (
         REM latest version installed by the autoupdater
         "%~dp0node.exe" %DEV_SUSPEND% "%~dp0%BIN_NAME%.js" %*
     ) else if exist %LATEST_BINPATH% (
         REM if an autoupdater version exists and this is not it, run that instead
-        set SFDX_REDIRECTED=1
         %LATEST_BINPATH% %*
     ) else if exist "%~dp0..\client\bin\node.exe" (
         REM installer version
