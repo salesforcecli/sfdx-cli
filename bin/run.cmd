a318d0be9665906186f815b374a94f40a5f4a23c
Bob Bergman <rbergman@salesforce.com>
2019-03-06 11:17:42 -0700

    v6->v7 launch script redirection support

--- bin/run.cmd
+++ bin/run.cmd
@@ -12,7 +12,12 @@ set SFDX_BINPATH="%~dp0%BIN_NAME%.cmd"
 set LATEST_BINPATH="%LOCALAPPDATA%\%BIN_NAME%\client\bin\%BIN_NAME%.cmd"
 if "%SFDX_INSTALLER%"=="true" (
     REM installer/update that shipped its own node binary
-    if "%SFDX_REDIRECTED%" == "1" (
+    set "UPDATED="
+    REM if redirected from a v7 script
+    IF "%SFDX_REDIRECTED%" == "1" set UPDATED=1
+    REM if redirected form a v6 script
+    IF /I %LATEST_BINPATH% == %SFDX_BINPATH% set UPDATED=1
+    IF defined UPDATED (
         REM latest version installed by the autoupdater
         "%~dp0node.exe" %DEV_SUSPEND% "%~dp0%BIN_NAME%.js" %*
     ) else if exist %LATEST_BINPATH% (
