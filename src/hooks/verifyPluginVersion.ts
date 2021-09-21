ecdf4e65d39028b738d49eb4ceb3c326c053fe13
Benjamin Maggi <bmaggi@salesforce.com>
2021-09-21 14:22:36 -0300

    docs: improve error message

--- src/hooks/verifyPluginVersion.ts
+++ src/hooks/verifyPluginVersion.ts
@@ -13,7 +13,8 @@ const BANNED_PLUGINS: Record<string, string> = {
   salesforcedx: `The salesforcedx plugin is deprecated.
     Installing it manually via 'sfdx plugins:install salesforcedx' is no longer supported and can result in duplicate commands and outdated plugins.
     See https://github.com/forcedotcom/cli/issues/1016 for more information about this change.`,
-  'sfdx-cli': 'Installing sfdx-cli is not allowed.',
+  'sfdx-cli':
+    "'sfdx-cli' cannot be installed as a plugin. If you are trying to install an older version of the cli, visit https://sfdc.co/install-older-cli-versions for instructions",
 };
 const MIN_VERSION = '45.8.0';
 
