97791ad51347c72c85227e01f058f4d47757f8c7
Rodrigo Espinosa de los Monteros <1084688+RodEsp@users.noreply.github.com>
2021-09-30 12:11:38 -0400

    feat: use npm postinstall script to install sf

--- scripts/include-sf.js
+++ scripts/include-sf.js
@@ -4,13 +4,14 @@ const path = require('path');
 const shelljs = require('shelljs');
 const fs = require('fs');
 
-const sfUnixPath = 'node_modules/@salesforce/cli/bin/run';
+const sfGlobalPath = path.join(shelljs.exec('npm list -g --depth 0 | head -1'), 'node_modules', '@salesforce', 'cli');
+
+// Copy sf to the bin/cli dir of sfdx
+shelljs.mv('-f', sfGlobalPath, path.join(process.cwd(), 'bin', 'sf-cli'));
+
+const sfUnixPath = 'bin/sf-cli/bin/run';
 const sfBin = path.join('bin', 'sf');
 const sfdxBin = path.join('bin', 'sfdx');
-
-const sfWinPath = 'node_modules\\@salesforce\\cli\\bin\\run';
-const sfCmd = path.join('bin', 'sf.cmd');
-const sfdxCmd = path.join('bin', 'sfdx.cmd');
 
 console.log(`---- Including SF ----`);
 console.log(`  Updating ${sfdxBin} with references to sf`);
@@ -23,6 +24,10 @@ const binContents = fs
 console.log(`  Writing ${sfBin}`);
 fs.writeFileSync(sfBin, binContents);
 shelljs.chmod('+x', sfBin);
+
+const sfWinPath = 'bin\\sf-cli\\bin\\run';
+const sfCmd = path.join('bin', 'sf.cmd');
+const sfdxCmd = path.join('bin', 'sfdx.cmd');
 
 console.log(`  Updating ${sfCmd} with references to sf`);
 const cmdContents = fs
