880f47a193c74dc59590cc42acecd58d2e76773e
Eric Willhoit <ewillhoit@salesforce.com>
2021-12-08 16:27:41 -0600

    fix: fork and env var

--- scripts/post-install-release-notes.js
+++ scripts/post-install-release-notes.js
@@ -1,9 +1,11 @@
 var shell = require('shelljs');
 
+if (process.env.SFDX_HIDE_RELEASE_NOTES === 'true') return;
+
 try {
   var executable = process.platform === 'win32' ? 'run.cmd' : 'run';
 
   shell.exec(`${__dirname}/../bin/${executable} whatsnew --hook`);
 } catch (err) {
-  shell.exit(0);
+  return;
 }
