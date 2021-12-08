adca34bb7be205075ef847a36ebe4e1280ead224
Eric Willhoit <ewillhoit@salesforce.com>
2021-12-08 15:11:19 -0600

    fix: again

--- scripts/post-install-release-notes.js
+++ scripts/post-install-release-notes.js
@@ -3,7 +3,7 @@ var shell = require('shelljs');
 try {
   var executable = process.platform === 'win32' ? 'run.cmd' : 'run';
 
-  shell.exec(`node ${__dirname}/../bin/${executable} whatsnew --hook`);
+  shell.exec(`${__dirname}/../bin/${executable} whatsnew --hook`);
 } catch (err) {
   shell.exit(0);
 }
