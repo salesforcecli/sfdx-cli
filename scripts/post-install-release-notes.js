bcec69ea0ff08c2a1e4b3f565f3650edf16a5295
Eric Willhoit <ewillhoit@salesforce.com>
2021-12-09 16:15:39 -0600

    fix: spawnSync instead

--- scripts/post-install-release-notes.js
+++ scripts/post-install-release-notes.js
@@ -1,13 +1,32 @@
 #!/usr/bin/env node
 
-const execSync = require('child_process').execSync;
+// const execSync = require('child_process').execSync;
+const spawnSync = require('child_process').spawnSync;
 
 if (process.env.SFDX_HIDE_RELEASE_NOTES === 'true') return;
 
 var executable = process.platform === 'win32' ? 'run.cmd' : 'run';
 
 try {
-  execSync(`${__dirname}/../bin/${executable} whatsnew --hook`, { stdio: 'inherit' });
+  spawnSync(`${__dirname}/../bin/${executable}`, ['whatsnew', '--hook'], { stdio: 'inherit' });
 } catch (e) {
   process.exit(0);
 }
+
+// try {
+//   execSync(`${__dirname}/../bin/${executable} whatsnew --hook`, { stdio: 'inherit' });
+// } catch (e) {
+//   process.exit(0);
+// }
+
+// var shell = require('shelljs');
+
+// if (process.env.SFDX_HIDE_RELEASE_NOTES === 'true') return;
+
+// try {
+//   var executable = process.platform === 'win32' ? 'run.cmd' : 'run';
+
+//   shell.exec(`${__dirname}/../bin/${executable} whatsnew --hook`);
+// } catch (err) {
+//   shell.exit(0);
+// }
