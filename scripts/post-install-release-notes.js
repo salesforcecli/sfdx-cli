c85e0aad78481e730d4cda4e50e4ad22c5d737f2
Eric Willhoit <ewillhoit@salesforce.com>
2021-12-13 15:37:58 -0600

    fix: pipe error

--- scripts/post-install-release-notes.js
+++ scripts/post-install-release-notes.js
@@ -1,18 +1,38 @@
 #!/usr/bin/env node
 
-const execFileSync = require('child_process').execFileSync;
-
-if (process.env.SFDX_HIDE_RELEASE_NOTES === 'true') return;
+const { spawn } = require('child_process');
 
 var executable = process.platform === 'win32' ? 'run.cmd' : 'run';
 
-try {
-  execFileSync(`${__dirname}/../bin/${executable}`, ['whatsnew', '--hook'], { stdio: 'inherit' });
-} catch (e) {
-  console.log('ERROR_CAUGHT');
-  process.exit(0);
-  console.log('AFTER_EXIT');
-}
+var cmd = spawn(`${__dirname}/../bin/${executable}`, ['whatsnew', '--hook'], { stdio: ['pipe', 'inherit', 'pipe'] });
+
+cmd.on('close', (code) => {
+  console.log('Exit Code from close:', code);
+  // process.exit(0);
+});
+
+cmd.on('exit', (code) => {
+  console.log('Exit Code from exit:', code);
+  // process.exit(0);
+});
+
+// ---------------------------------------
+
+// const execFileSync = require('child_process').execFileSync;
+
+// if (process.env.SFDX_HIDE_RELEASE_NOTES === 'true') return;
+
+// var executable = process.platform === 'win32' ? 'run.cmd' : 'run';
+
+// try {
+//   execFileSync(`${__dirname}/../bin/${executable}`, ['whatsnew', '--hook'], { stdio: 'inherit' });
+// } catch (e) {
+//   console.log('ERROR_CAUGHT');
+//   process.exit(0);
+//   console.log('AFTER_EXIT');
+// }
+
+// ----------------------------------------
 
 // Below would exit correctly, but did not have colored output
 
@@ -23,7 +43,8 @@ try {
 // try {
 //   var executable = process.platform === 'win32' ? 'run.cmd' : 'run';
 
-//   shell.exec(`${__dirname}/../bin/${executable} whatsnew --hook`);
+//   var cmd = shell.exec(`${__dirname}/../bin/${executable} whatsnew --hook`);
+
 // } catch (err) {
 //   shell.exit(0);
 // }
