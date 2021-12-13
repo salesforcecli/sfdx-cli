9ba5b4089b10287f6703150e801bf1d3da5f1053
Eric Willhoit <ewillhoit@salesforce.com>
2021-12-13 16:39:56 -0600

    fix: timeout on shelljs

--- scripts/post-install-release-notes.js
+++ scripts/post-install-release-notes.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
-const { spawn } = require('child_process');
+// const { spawn } = require('child_process');
 
-var executable = process.platform === 'win32' ? 'run.cmd' : 'run';
+// var executable = process.platform === 'win32' ? 'run.cmd' : 'run';
 
-var cmd = spawn(`${__dirname}/../bin/${executable}`, ['whatsnew', '--hook'], {
-  stdio: ['ignore', 'inherit', 'pipe'],
-  timeout: 10000,
-  windowsHide: false,
-});
+// var cmd = spawn(`${__dirname}/../bin/${executable}`, ['whatsnew', '--hook'], {
+//   stdio: ['ignore', 'inherit', 'pipe'],
+//   timeout: 10000,
+//   windowsHide: false,
+// });
 
 // try windowsHide?
 
@@ -21,11 +21,11 @@ var cmd = spawn(`${__dirname}/../bin/${executable}`, ['whatsnew', '--hook'], {
 //   console.log(data.toString());
 // });
 
-cmd.stderr.on('data', (error) => {
-  console.log('STDERR:');
-  console.log(error.toString());
-  process.exit(0);
-});
+// cmd.stderr.on('data', (error) => {
+//   console.log('STDERR:');
+//   console.log(error.toString());
+//   process.exit(0);
+// });
 
 // // 'close' would only ever fire when the streams are all terminated
 // cmd.on('close', (code) => {
@@ -34,10 +34,10 @@ cmd.stderr.on('data', (error) => {
 // });
 
 // // 'exit' fires whether or not the stream are finished
-cmd.on('exit', (code) => {
-  console.log('Exit Code from exit:', code);
-  process.exit(0);
-});
+// cmd.on('exit', (code) => {
+//   console.log('Exit Code from exit:', code);
+//   process.exit(0);
+// });
 
 // ---------------------------------------
 
@@ -59,15 +59,14 @@ cmd.on('exit', (code) => {
 
 // Below would exit correctly, but did not have colored output
 
-// var shell = require('shelljs');
+var shell = require('shelljs');
 
-// if (process.env.SFDX_HIDE_RELEASE_NOTES === 'true') return;
+if (process.env.SFDX_HIDE_RELEASE_NOTES === 'true') return;
 
-// try {
-//   var executable = process.platform === 'win32' ? 'run.cmd' : 'run';
+try {
+  var executable = process.platform === 'win32' ? 'run.cmd' : 'run';
 
-//   var cmd = shell.exec(`${__dirname}/../bin/${executable} whatsnew --hook`);
-
-// } catch (err) {
-//   shell.exit(0);
-// }
+  shell.exec(`${__dirname}/../bin/${executable} whatsnew --hook`, { timeout: 10000 });
+} catch (err) {
+  shell.exit(0);
+}
