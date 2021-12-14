4fd04c1ab8862ecd4d797ffd818b9380643cb7dc
Eric Willhoit <ewillhoit@salesforce.com>
2021-12-14 15:03:53 -0600

    fix: windows update hook tweaks

--- src/hooks/displayReleaseNotes.ts
+++ src/hooks/displayReleaseNotes.ts
@@ -5,6 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+import { join } from 'path';
 import { spawn } from 'child_process';
 import { Hook } from '@oclif/config';
 import { cli } from 'cli-ux';
@@ -12,9 +13,11 @@ import { cli } from 'cli-ux';
 if (process.env.SFDX_HIDE_RELEASE_NOTES === 'true') process.exit(0);
 
 const hook: Hook.Update = () => {
-  const executable = process.platform === 'win32' ? 'run.cmd' : 'run';
+  // NOTE: This is `sfdx.cmd` here and not `run.cmd` because it gets renamed here:
+  // https://github.com/salesforcecli/sfdx-cli/blob/4428505ab69aa6e21214dba96557e2ce396a82e0/src/hooks/postupdate.ts#L62
+  const executable = process.platform === 'win32' ? 'sfdx.cmd' : 'run';
 
-  const cmd = spawn(`${__dirname}/../../bin/${executable}`, ['whatsnew', '--hook'], {
+  const cmd = spawn(join(__dirname, '..', '..', 'bin', executable), ['whatsnew', '--hook'], {
     stdio: ['ignore', 'inherit', 'pipe'],
     timeout: 10000,
   });
--- src/hooks/testHook.js
+++
@@ -1,3 +0,0 @@
-import hook from './displayReleaseNotes';
-
-hook();
