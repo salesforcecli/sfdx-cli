b7d4a5e13ece5eab4ec5e6afe80cda44c04a32e1
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2022-01-24 13:03:46 -0600

    feat: warn people about salesforcedx during update

---
+++ src/hooks/salesforcedx-check.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import * as path from 'path';
+import Plugins from '@oclif/plugin-plugins';
+import { Config } from '@oclif/config';
+import { AnyJson, get } from '@salesforce/ts-types';
+import { cli } from 'cli-ux';
+import { compareVersions } from '../versions';
+
+const salesforcedxError = `The salesforcedx plugin is deprecated.  Using it results in very old versions of commands running.
+    Uninstall it with 'sfdx plugins:uninstall salesforcedx'.
+    See https://github.com/forcedotcom/cli/blob/main/releasenotes/sfdx/README.md#71063-june-17-2021 for more information about this change.`;
+
+const MAX_VERSION = '7.107.0';
+
+const hook = async (): Promise<void> => {
+  // PART 1: is the CLI recent enough that we don't allow salesforcedx?
+  const root = path.resolve(__dirname, '..', '..');
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const pjson = require(path.resolve(root, 'package.json')) as AnyJson;
+  const config = new Config({ name: get(pjson, 'oclif.bin') as string | undefined, root });
+  await config.load();
+
+  // early exit for old CLIs that shouldn't bother inspecting their plugins
+  if (compareVersions(config.version, MAX_VERSION) < 0) {
+    return;
+  }
+
+  // PART 2: is the salesforcedx plugin installed?
+  const plugins = new Plugins(config);
+
+  const installedUserPlugins = (await plugins.list())
+    .filter((plugin) => plugin.type === 'user')
+    .map((plugin) => plugin.name);
+  cli.log(`user plugins are ${installedUserPlugins.join(', ')}`);
+
+  if (installedUserPlugins.includes('salesforcedx')) {
+    cli.warn(salesforcedxError);
+  }
+};
+
+export default hook;
