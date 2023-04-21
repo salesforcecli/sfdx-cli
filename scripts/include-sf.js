a65a0baa16a0559788d8dc41efd0ad11747cac13
Eric Willhoit <ewillhoit@salesforce.com>
2023-04-21 16:14:20 -0500

    chore: ignore prereleases

--- scripts/include-sf.js
+++ scripts/include-sf.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 // Get the latest version that starts with 1 (we do not want to ever bundle version 2 of @salesforce/cli)
 // We cannot use @salesforce/cli@^1 because that will not get versions higher than what is tagged as "latest"
 const versions = JSON.parse(shelljs.exec('npm view @salesforce/cli versions --json').stdout.trim());
-const latestVersion = versions.reverse().find((v) => v.startsWith('1.'));
+const latestVersion = versions.reverse().find((v) => v.startsWith('1.') && !v.includes('-'));
 shelljs.exec(`npm install @salesforce/cli@${latestVersion} -g`);
 
 const npmGlobalInstallPath = shelljs.exec('npm list -g --depth 0 | head -1').stdout.trim();
