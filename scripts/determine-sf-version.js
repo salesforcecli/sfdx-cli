b45cc811380eb5c67bd7a9b14823453194d7a071
Eric Willhoit <ewillhoit@salesforce.com>
2023-05-17 09:54:04 -0500

    chore: better sf bundle lookup

--- scripts/determine-sf-version.js
+++ scripts/determine-sf-version.js
@@ -7,14 +7,17 @@ const getVersion = () => {
   // Get all npm dist-tags for sfdx-cli
   const distTags = JSON.parse(shelljs.exec('npm view sfdx-cli dist-tags --json', { silent: true }).stdout.trim());
 
-  // Find the dist-tag that matches the sfdx-cli package.json version
-  const distTag = Object.entries(distTags).find(([, version]) => version === sfdxVersion)[0];
+  // Attempt to find a dist-tag that matches the sfdx-cli package.json version
+  const distTag = Object.entries(distTags).find(([, version]) => version === sfdxVersion);
 
-  // Now get the version of @salesforce/cli for the same dist-tag
-  let sfVersion = shelljs.exec(`npm view @salesforce/cli@${distTag} version`, { silent: true }).stdout.trim();
+  // If we found a dist-tag, get the version of @salesforce/cli for the same dist-tag
+  // Note: the --silent flag causes the command to return an empty string if the version is not found
+  let sfVersion = distTag?.[0]
+    ? shelljs.exec(`npm view @salesforce/cli@${distTag[0]} version --silent`, { silent: true }).stdout.trim()
+    : undefined;
 
-  // Make sure that the version of @salesforce/cli does not start with 2
-  if (sfVersion.startsWith('2')) {
+  // Make sure that sfVersion is set and it does not start with 2
+  if (!sfVersion || sfVersion.startsWith('2')) {
     // If it does, get the latest version that starts with 1 (we do not want to ever bundle version 2 of @salesforce/cli)
     // We cannot use @salesforce/cli@^1 because that will not get versions higher than what is tagged as "latest"
     const versions = JSON.parse(
